5761a0cebf140ba152ee9f766a65d999b2b32492
ilyasaitbamou <ilyasaitbamou2005@gmail.com>
2026-03-06 16:24:57 +0000

    release new versions

---
+++ functions.php
@@ -0,0 +1,62 @@
+<?php 
+// challange 1
+function  greetUser($name) {
+    echo $name;
+}
+
+$name = "Ilyas\n";
+greetUser($name);
+
+// challange 2
+function calculateArea($width, $height) {
+    return $width * $height;
+}
+
+$width = 20;
+$height = 10;
+$totalArea = calculateArea($width, $height);
+echo "The total area is $totalArea square units\n";
+
+// challange 3
+function IsAdult($age) {
+
+    if ($age > 0) {
+        if ($age < 18) {
+        return false; 
+
+        } else {
+            return true;
+        }
+    } 
+    return null; 
+
+}
+
+$age = 20;
+if (IsAdult($age)) {
+    echo "Access Granted.\n";
+} else { 
+    echo "Access Denied\n";
+}
+
+// challange 4
+function  multiplyNumbers($a, $b)  {
+    if (is_int($a) && is_int($b)) {
+        return $a * $b ;
+}
+    return "\nError: Invalid Input\n";
+}
+echo multiplyNumbers(5, 10);
+echo multiplyNumbers(5, "apple");
+
+// challange 5
+function manualReverse($text) {
+    $reversed_text = "";
+    for ($i=0; $i < strlen($text); $i++) { 
+        $reversed_text .= $text[strlen($text) - 1 - $i];
+    }
+    return $reversed_text;
+}
+$text = "mohamed";
+echo manualReverse($text);
+?>
